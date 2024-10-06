9c30a1785208185052697c5499fd4597aee2c9b1
rupert foley <rupert.foley@gmail.com>
2024-10-06 15:12:12 +0100

    added leafletjs map to reporting fields

---
+++ wwwroot/js/leafletmap.js
@@ -0,0 +1,5 @@
+﻿var map = L.map('reportCoordinatesInput').setView([52.2371, -0.8944], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
