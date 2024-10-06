7eb3ed082ab382ec60a3079fa7460b263d263481
rupert foley <rupert.foley@gmail.com>
2024-10-06 15:21:48 +0100

    added drop pin system to leaflet map

--- wwwroot/js/leafletmap.js
+++ wwwroot/js/leafletmap.js
@@ -3,3 +3,20 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var marker;
+
+function dropMarker(event) {
+    var latitude = event.latlng.lat;;
+    var longitude = event.latlng.lng;
+
+    
+
+    if (marker != undefined) {
+        map.removeLayer(marker);
+    }
+
+    marker = L.marker([latitude, longitude]).addTo(map);
+}
+
+map.on("click", dropMarker);
